12ec2b5794586175ec373c3417ee25fc225777f5
VITO VINCENTDO <U068863@bca.co.id>
2022-11-14 19:53:06 +0700

    Update CRUD on graph api demo v2

--- graph_api_demo_V2.py
+++ graph_api_demo_V2.py
@@ -34,7 +34,6 @@ def msal_cache_accounts(clientID, authority):
     app = msal.PublicClientApplication(
         client_id=clientID, authority=authority, token_cache=cache)
     accounts = app.get_accounts()
-    print(accounts)
     return accounts
 
 def msal_delegated_refresh(clientID, scope, authority, account):
@@ -85,9 +84,24 @@ def msal_jwt_expiry(accessToken):
     print("Token Expires at: " + str(tokenExpiry))
     return tokenExpiry
 
-def msgraph_request(resource, requestHeaders):
+def msgraph_request_get(resource, requestHeaders):
     # Request
     results = requests.get(resource, headers=requestHeaders).json()
+    return results
+
+def msgraph_request_post(resource, requestHeaders, jsonObject):
+    # Request
+    results = requests.post(resource, headers=requestHeaders, json=jsonObject).json()
+    return results
+
+def msgraph_request_patch(resource, requestHeaders, newData):
+    # Request
+    results = requests.patch(resource, headers=requestHeaders, data=newData).json()
+    return results
+
+def msgraph_request_delete(resource, requestHeaders):
+    # Request
+    results = requests.delete(resource, headers=requestHeaders)
     return results
 
 accounts = msal_cache_accounts(clientID, authority)
@@ -114,12 +128,12 @@ else:
         msal_jwt_expiry(result["access_token"])
 
 # Query AAD Users based on voice query using DisplayName
-print(result['access_token'])
-print(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/34bf6b0f-621e-4159-b916-d16e153678bd/items")
-requestHeaders = {'Authorization': 'Bearer ' + result["access_token"],'Content-Type': 'application/json'}
-queryResults = msgraph_request(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/34bf6b0f-621e-4159-b916-d16e153678bd/items",requestHeaders)
-
-print(json.dumps(queryResults, indent=2))
+# print(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/db38cd50-ad17-49c6-a06f-99d9ed17ffdf/items?expand=fields")
+requestHeaders = {'Authorization': 'Bearer ' + result["access_token"],'Content-Type': 'application/json', 'Accept': 'application/json'}
+print('======================================')
+print('Get All Master List License Data')
+print('======================================')
+queryResults = msgraph_request_get(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/69C54909-36C1-483A-9C3C-006B41CD6355/items?expand=fields&$top=10000",requestHeaders)
 
 # Force Token Refresh
 result =  msal_delegated_refresh_force(clientID, scope, authority, myAccount)
@@ -131,3 +145,103 @@ else:
         msal_jwt_expiry(result["access_token"])   
 
 print(json.dumps(queryResults, indent=2))
+print('======================================\n')
+
+print('===========================================')
+print('Get Single Item From Master List License')
+print('===========================================')
+queryResults = msgraph_request_get(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/69C54909-36C1-483A-9C3C-006B41CD6355/items/1?expand=fields",requestHeaders)
+
+
+# Force Token Refresh
+result =  msal_delegated_refresh_force(clientID, scope, authority, myAccount)
+if result is None:
+    # Get a new Access Token using the Device Code Flow
+    result = msal_delegated_device_flow(clientID, scope, authority)
+else:
+    if result["access_token"]:
+        msal_jwt_expiry(result["access_token"])   
+
+print(json.dumps(queryResults, indent=2))
+print('===========================================\n')
+
+print('===========================================')
+print('Create New Item Into Master List License')
+print('===========================================')
+
+TestData = {
+    "fields": {
+        "Title": "GIMP",
+        "field_2": "Open Source (GIMP Development Team",
+        "field_3": "2.10.30",
+        "field_4": "Free",
+        "field_5": "-",
+        "field_6": "Image Editor",
+        "field_7": "Open file .tiff, convert .tiff ke .pdf dan sebaliknya, edit file .tiff. Bisa menjadi alternatif aplikasi MODI",
+        "field_8": "29-Mar-22",
+        "field_9": "Hendro Purwanto",
+        "field_10": "Yes",
+        "field_11": "ALL BCA",
+        "field_12": ""
+    }
+}
+
+queryResults = msgraph_request_post(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/69C54909-36C1-483A-9C3C-006B41CD6355/items",requestHeaders,TestData)
+
+
+# Force Token Refresh
+result =  msal_delegated_refresh_force(clientID, scope, authority, myAccount)
+if result is None:
+    # Get a new Access Token using the Device Code Flow
+    result = msal_delegated_device_flow(clientID, scope, authority)
+else:
+    if result["access_token"]:
+        msal_jwt_expiry(result["access_token"])   
+
+print(json.dumps(queryResults, indent=2))
+print('===========================================\n')
+
+print('===========================================')
+print('Create New Item Into Master List License')
+print('===========================================')
+
+newData = {
+    "Title": "Beyond Compare (Test)",
+    "field_5": "- (Test)"
+}
+
+print(json.dumps(newData))
+
+queryResults = msgraph_request_patch(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/69C54909-36C1-483A-9C3C-006B41CD6355/items/1/fields",requestHeaders,json.dumps(newData))
+
+
+# Force Token Refresh
+result =  msal_delegated_refresh_force(clientID, scope, authority, myAccount)
+if result is None:
+    # Get a new Access Token using the Device Code Flow
+    result = msal_delegated_device_flow(clientID, scope, authority)
+else:
+    if result["access_token"]:
+        msal_jwt_expiry(result["access_token"])   
+
+print(json.dumps(queryResults, indent=2))
+print('===========================================\n')
+
+print('===========================================')
+print('Delete an Item From Master List License')
+print('===========================================')
+
+queryResults = msgraph_request_delete(graphURI + "/v1.0/sites/bcaoffice365.sharepoint.com,3983b9b4-f1ea-4771-91cb-fbbcab357e2a,ff059f1c-9c7c-49d4-ac63-b73d11cd1c1f/lists/69C54909-36C1-483A-9C3C-006B41CD6355/items/9",requestHeaders)
+
+
+# Force Token Refresh
+result =  msal_delegated_refresh_force(clientID, scope, authority, myAccount)
+if result is None:
+    # Get a new Access Token using the Device Code Flow
+    result = msal_delegated_device_flow(clientID, scope, authority)
+else:
+    if result["access_token"]:
+        msal_jwt_expiry(result["access_token"])   
+
+print(queryResults)
+print('===========================================\n')
